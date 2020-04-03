313ec85f39cb05fa94336af516c9457403eb9ff2
nullchinchilla <nullchinchilla@pm.me>
2020-04-02 22:21:42 -0400

    fix

--- UPDATE-DAEMON.sh
+++ UPDATE-DAEMON.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PREFIX="https://binaries.geph.io"
-export VERSION="v0.20.0"
+export VERSION="v0.20.1"
 
 PREBUILD="./prebuild"
 # ABI names must match with arguments provided to android.defaultConfig.ndk.abiFilters
