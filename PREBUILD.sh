49a65fdddafde914df671291357d36dd75dd9954
nullchinchilla <nullchinchilla@pm.me>
2023-10-14 21:20:56 -0400

    Enable x86 and x86_64 prebuild, update app version to 4.9.5, and update gephgui subproject commit

--- PREBUILD.sh
+++ PREBUILD.sh
@@ -9,14 +9,14 @@ PREBUILD="./prebuild"
 # ABI names must match with arguments provided to android.defaultConfig.ndk.abiFilters
 ARM_DIR=$PREBUILD"/armeabi-v7a"
 ARM64_DIR=$PREBUILD"/arm64-v8a"
-#X86_DIR=$PREBUILD"/x86"
-#X86_64_DIR=$PREBUILD"/x86_64"
+X86_DIR=$PREBUILD"/x86"
+X86_64_DIR=$PREBUILD"/x86_64"
 TARGET="libgeph.so"
 
 mkdir -p $ARM_DIR
-#mkdir -p $X86_DIR
+mkdir -p $X86_DIR
 mkdir -p $ARM64_DIR
-#mkdir -p $X86_64_DIR
+mkdir -p $X86_64_DIR
 
 # Function to download the file only if local and remote file sizes differ or the local file doesn't exist
 download_if_needed() {
@@ -35,3 +35,5 @@ download_if_needed() {
 
 download_if_needed "$PREFIX/geph4-client-android-armv7" $ARM_DIR/$TARGET
 download_if_needed "$PREFIX/geph4-client-android-aarch64" $ARM64_DIR/$TARGET
+#download_if_needed "$PREFIX/geph4-client-android-amd64" $X86_64_DIR/$TARGET
+#download_if_needed "$PREFIX/geph4-client-android-i386" $X86_DIR/$TARGET
